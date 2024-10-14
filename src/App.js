4e2222a25ac6e035cfc4c3c99268a3ad8316a0d1
martin0995 <martin@papermoon.io>
2024-10-14 12:27:55 +0200

    created basic connect rpject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import WormholeConnect from '@wormhole-foundation/wormhole-connect';
+
+const config = {
+	env: 'testnet',
+	// networks: ['sepolia', 'arbitrum_sepolia', 'base_sepolia', 'fuji', 'solana', 'basegoerli', 'moonbasealpha'],
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <WormholeConnect config={config} />;
 }
 
 export default App;
